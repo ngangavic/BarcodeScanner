c7d128cb6c425942af6643eb15672d7f9b6a3796
ngangavic <ngangavictor10@gmail.com>
2019-10-14 15:31:15 +0300

    set buttons

--- app/src/main/java/com/example/barcodescanner/MainActivity.java
+++ app/src/main/java/com/example/barcodescanner/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.barcodescanner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonScan,buttonTake;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonScan=findViewById(R.id.buttonScancode);
+        buttonTake=findViewById(R.id.buttonTakePicture);
+
+        buttonScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        buttonTake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
