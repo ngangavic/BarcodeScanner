63e53fa903d0c963fe00ebe6efc73931e8928fd8
ngangavic <ngangavictor10@gmail.com>
2019-10-14 21:00:17 +0300

    final code

--- app/src/main/java/com/example/barcodescanner/MainActivity.java
+++ app/src/main/java/com/example/barcodescanner/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.barcodescanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,14 +21,14 @@ public class MainActivity extends AppCompatActivity {
         buttonScan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+startActivity(new Intent(MainActivity.this,ScancodeActivity.class));
             }
         });
 
         buttonTake.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                startActivity(new Intent(MainActivity.this,TakeImageActivity.class));
             }
         });
     }
